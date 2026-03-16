f3d5e9eb6f45517e0816493d77245d472f3a1a38
Agbonyin Clement <agbonyinclement@gmail.com>
2026-03-16 17:47:16 -0500

    History Blog

---
+++ script.js
@@ -0,0 +1,75 @@
+
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-app.js";
+  import { getFirestore, collection, getDocs } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-firestore.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyDcudItAwVIumhNwU9wgbBqXpC45fGgp8w",
+    authDomain: "klemcode.firebaseapp.com",
+    projectId: "klemcode",
+    storageBucket: "klemcode.firebasestorage.app",
+    messagingSenderId: "59149627541",
+    appId: "1:59149627541:web:2e30ff389ac4b50be02b9c"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const db= getFirestore(app)
+
+  console.log(db);
+
+  const colRef = collection(db, "klemblog");
+ console.log(colRef);  
+const blogArr=[];
+
+async function getAllBlogs() {
+    try {
+        
+        const blogs= await getDocs(colRef)
+        console.log(blogs);
+
+        blogs.forEach((element)=> {
+            
+            const data={id: element.id, ...element.data()}
+            blogArr.push(data)
+            console.log(blogArr);
+        });
+        
+    
+        function displayAllBlogs(){
+let display =document.getElementById("display");
+
+
+blogArr.forEach((blog)=>{
+ display.innerHTML +=`
+    <div style="border: 1px solid green; width:500px; padding=25px; margin: 10px; border-radius:4%">
+
+    <img src="${blog.image}" Alt="" style="width:400px; border-radius:5%">
+     <h3 style="padding=10px; margin: 6px">Author: ${blog.author}</h3>
+    <h1 style="padding=10px; margin: 6px"><a href="./singleblog.html?id=${blog.id}"> ${blog.title}</a></h1>
+    <p style="padding=10px; margin: 6px">${blog.content}</p>
+    <p style="padding=10px; margin: 6px">${blog.isPublished ? "Published" : "Not Yet Published" }</p>
+   
+    
+    </div>
+ `
+
+})
+}
+displayAllBlogs()
+
+
+    } catch (error) {
+        console.log(error);
+        
+        
+    }
+
+    
+}
+
+getAllBlogs();
+
